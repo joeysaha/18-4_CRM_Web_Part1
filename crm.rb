d0be739ddfaaa80e72db7fac8c9514e092160a26
Joey Saha <joeysaha@hotmail.com>
2018-08-12 17:41:38 -0400

    crm stuff

---
+++ crm.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require_relative 'contact'
+ get '/' do
+  erb :index
+end
+
+get '/contacts' do
+  @contacts = Contact.all
+  erb :contacts
+end
+
+get '/about' do
+  @country = "canada"
+  erb :about
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
