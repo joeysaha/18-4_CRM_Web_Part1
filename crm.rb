aca6b30efff1072f54ace37b35071115867994c9
Joey Saha <joeysaha@hotmail.com>
2018-08-12 22:54:53 -0400

    new contact finished

--- crm.rb
+++ crm.rb
@@ -10,6 +10,10 @@ get '/contacts' do
   erb :contacts
 end
 
+get '/contacts/new' do
+  erb :new
+end
+
 get '/contacts/:id' do
   @contact = Contact.find_by({ id: params[:id].to_i })
   if @contact
@@ -24,8 +28,15 @@ get '/about' do
   erb :about
 end
 
-get '/new' do
-  erb :new
+
+post '/contacts' do
+  Contact.create(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+  )
+  redirect to('/contacts')
 end
 
 after do
