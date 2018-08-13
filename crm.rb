429182efe11fa5a1622b1e8bc88a9fe2fa914306
Joey Saha <joeysaha@hotmail.com>
2018-08-13 00:45:25 -0400

    delete

--- crm.rb
+++ crm.rb
@@ -2,12 +2,16 @@ require 'sinatra'
 require_relative 'contact'
 
 get '/' do
-  erb :index
+  redirect to('/contacts')
 end
 
 get '/contacts' do
   @contacts = Contact.all
   erb :contacts
+end
+
+get '/contacts/' do
+  redirect to('/contacts')
 end
 
 get '/contacts/new' do
@@ -37,6 +41,42 @@ post '/contacts' do
     note:       params[:note]
   )
   redirect to('/contacts')
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find_by(params[:id].to_i)
+  if @contact
+    p @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 after do
